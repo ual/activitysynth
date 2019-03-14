d82d0757dd063c858b519077bbbfc6539540ede8
Max Gardner <gardner.max@gmail.com>
2019-03-13 21:05:16 -0700

    s3 version working

--- activitysynth/run.py
+++ activitysynth/run.py
@@ -1,54 +1,67 @@
 import orca
 import pandas as pd
 import warnings
-import os
-import boto3
 
 import urbansim_templates
-
 from activitysynth.scripts import models, datasources, variables
 
 warnings.simplefilter('ignore')
-
-accessibilities_mode = 'recompute'
-orca.add_injectable('data_directory', '/home/data/spring_2019/')
+accessibilities_mode = 'compute'
+year = 2010
+output_tables = [
+    'parcels', 'buildings', 'jobs', 'persons', 'households',
+    'establishments', 'rentals', 'units']
+output_bucket = 'urbansim-outputs'
+beam_bucket = 'urbansim-beam'
+asynth_data_dir = './data/'
+fname_walk = 'walk_net_vars.csv'
+fname_drive = 'drive_net_vars.csv'
 
 
 if __name__ == "__main__":
 
-    # model_steps = [
-    #     'initialize_network_small',
-    #     'initialize_network_walk'
-    # ]
+    # initialize network
+    model_steps = [
+        'initialize_network_small',
+        'initialize_network_walk'
+    ]
 
-    # orca.run(model_steps)
-    year = 2010
+    orca.run(model_steps)
 
-    # if accessibilities_mode == 'recompute':
-    #     model_steps = [
-    #         'network_aggregations_small', 'network_aggregations_walk']
-    #     orca.run(model_steps)
+    if accessibilities_mode == 'compute':
+        model_steps = [
+            'network_aggregations_small', 'network_aggregations_walk']
+        orca.run(model_steps)
+        orca.get_table('nodeswalk').to_frame().to_csv(
+            asynth_data_dir + fname_walk)
+        orca.get_table('nodessmall').to_frame().to_csv(
+            asynth_data_dir + fname_drive)
 
-    # elif accessibilities_mode == 'precomputed':
-    #     access_vars_dir = '../ual_model_workspace/spring-2019-models/data/'
-    #     fname_walk = 'walk_net_vars.csv'
-    #     fname_drive = 'drive_net_vars.csv'
+    elif accessibilities_mode == 'precomputed':
+        walk_net_vars = pd.read_csv(
+            asynth_data_dir + fname_walk, index_col='osmid')
+        drive_net_vars = pd.read_csv(
+            asynth_data_dir + fname_drive, index_col='osmid')
+        orca.add_table('nodeswalk', walk_net_vars)
+        orca.add_table('nodessmall', drive_net_vars)
 
-    #     walk_net_vars = pd.read_csv(
-    #         access_vars_dir + fname_walk, index_col='osmid')
-    #     drive_net_vars = pd.read_csv(
-    #         access_vars_dir + fname_drive, index_col='osmid')
-    #     orca.add_table('nodeswalk', walk_net_vars)
-    #     orca.add_table('nodessmall', drive_net_vars)
+    model_steps = [
+        'wlcm_simulate', 'TOD_choice_simulate',
+        'TOD_distribution_simulate',
+        'auto_ownership_simulate', 'primary_mode_choice_simulate',
+        'generate_activity_plans']
 
-    # model_steps = [
-    #     'wlcm_simulate', 'TOD_choice_simulate',
-    #     'auto_ownership_simulate', 'primary_mode_choice_simulate',
-    #     'TOD_distribution_simulate', 'generate_activity_plans']
+    orca.run(model_steps)
 
-    # orca.run(model_steps)
+    # save tables to parquet on s3
+    # for table_name in output_tables:
+    #     df = orca.get_table(table_name).to_frame()
+    #     s3_url = 's3://{0}/{1}/{2}.parquet.gz'.format(
+    #         output_bucket, year, table_name)
+    #     df.to_parquet(s3_url, compression='gzip', engine='pyarrow')
 
-    s3 = boto3.resource('s3')
-    data = open('./data/urbansim_beam_plans.csv', 'rb')
-    s3.Bucket('urbansim-beam').put_object(
-        Key='activity_plans_{0}.csv'.format(year), Body=data)
+    # # save plans to parquet s3
+    # plans = orca.get_table('plans').to_frame()
+    # s3_url = 's3://{0}/{1}/{2}.parquet.gz'.format(
+    #     beam_bucket, year, 'activity_plans')
+    # plans.to_parquet(s3_url, compression='gzip')
--- activitysynth/scripts/datasources.py
+++ activitysynth/scripts/datasources.py
@@ -1,242 +1,90 @@
-# import orca
-# import numpy as np
-# import pandas as pd
-
-
-# # data documentation: https://berkeley.app.box.com/notes/282712547032
-
-
-# # Set data directory
-
-# d = '/home/data/fall_2018/'
-
-# if 'data_directory' in orca.list_injectables():
-#     d = orca.get_injectable('data_directory')
-
-
-# ############################################################
-
-# # Tables from MTC Bay Area UrbanSim
-
-# @orca.table(cache=True)
-# def parcels():
-#     df = pd.read_csv(
-#         d + 'parcel_attr.csv',
-#         index_col='primary_id',
-#         dtype={'primary_id': int, 'block_id': str})
-#     return df
-
-
-# @orca.table(cache=True)
-# def buildings():
-#     df = pd.read_csv(
-#         d + 'buildings_v2.csv',
-#         index_col='building_id', dtype={'building_id': int, 'parcel_id': int})
-#     df['res_sqft_per_unit'] = df['residential_sqft'] / df['residential_units']
-#     df['res_sqft_per_unit'][df['res_sqft_per_unit'] == np.inf] = 0
-#     return df
-
-
-# ############################################################
-
-# # Table of Rental Data from  Craigslist, bayarea_urbansim added by Arezoo
-
-# @orca.table(cache=True)
-# def craigslist():
-#     df = pd.read_csv(
-#         d + 'MTC_craigslist_listings_7-10-18.csv',
-#         index_col='pid', dtype={'pid': int})
-#     return df
-
-
-# @orca.table(cache=True)
-# def rentals():
-#     df = pd.read_csv(
-#         d + 'rentals_with_nodes.csv',
-#         index_col='pid', dtype={'pid': int, 'rent': float})
-#     return df
-
-
-# ############################################################
-
-# # Tables synthesized by Max Gardner
-
-# @orca.table(cache=True)
-# def units():
-#     df = pd.read_csv(
-#         d + 'units_v2.csv',
-#         index_col='unit_id', dtype={'unit_id': int, 'building_id': int})
-#     return df
-
-
-# @orca.table(cache=True)
-# def households():
-#     df = pd.read_csv(
-#         d + 'households_v2.csv',
-#         index_col='household_id', dtype={
-#             'household_id': int, 'block_group_id': str, 'state': str,
-#             'county': str, 'tract': str, 'block_group': str,
-#             'building_id': int, 'unit_id': int, 'persons': float})
-#     return df
-
-
-# @orca.table(cache=True)
-# def persons():
-#     df = pd.read_csv(
-#         d + 'persons_v3.csv',
-#         index_col='person_id', dtype={'person_id': int, 'household_id': int})
-#     return df
-
-
-# @orca.table(cache=True)
-# def jobs():
-#     df = pd.read_csv(
-#         d + 'jobs_v2.csv',
-#         index_col='job_id', dtype={'job_id': int, 'building_id': int})
-#     return df
-
-
-# ############################################################
-
-# # Tables from Sam Blanchard
-# @orca.table(cache=True)
-# def establishments():
-#     df = pd.read_csv(
-#         d + 'establishments_v2.csv',
-#         index_col='establishment_id', dtype={
-#             'establishment_id': int, 'building_id': int, 'primary_id': int})
-#     return df
-
-# ############################################################
-
-# # Broadcasts, a.k.a. merge relationships
-
-
-# orca.broadcast(
-#     'parcels', 'buildings', cast_index=True, onto_on='parcel_id')
-# orca.broadcast(
-#     'buildings', 'units', cast_index=True, onto_on='building_id')
-# orca.broadcast(
-#     'units', 'households', cast_index=True, onto_on='unit_id')
-# orca.broadcast(
-#     'households', 'persons', cast_index=True, onto_on='household_id')
-# orca.broadcast(
-#     'buildings', 'jobs', cast_index=True, onto_on='building_id')
-# orca.broadcast(
-#     'buildings', 'establishments', cast_index=True, onto_on='building_id')
-# orca.broadcast(
-#     'nodeswalk', 'parcels', cast_index=True, onto_on='node_id_walk')
-# orca.broadcast(
-#     'nodeswalk', 'rentals', cast_index=True, onto_on='node_id_walk')
-# orca.broadcast(
-#     'nodessmall', 'rentals', cast_index=True, onto_on='node_id_small')
-# orca.broadcast(
-#     'nodessmall', 'parcels', cast_index=True, onto_on='node_id_small')
-# # orca.broadcast(
-# #     'nodesbeam', 'parcels', cast_index=True, onto_on='node_id_beam')
-
 import orca
 import pandas as pd
 
-
 # data documentation: https://berkeley.app.box.com/notes/282712547032
 
-
-@orca.injectable('store', cache=True)
-def hdfstore(data_directory):
-    return pd.HDFStore((data_directory + "model_data.h5"), mode='r')
+orca.add_injectable(
+    's3_input_data_url', 's3://urbansim-baseyear-inputs/{0}.parquet.gz')
 
 
 @orca.table('parcels', cache=True)
-def parcels(store):
-    df = store['parcels']
+def parcels(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('parcels'))
     df.index.name = 'primary_id'
     return df
 
 
 @orca.table('buildings', cache=True)
-def buildings(store):
-    df = store['buildings']
+def buildings(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('buildings'))
     return df
 
 
 @orca.table('jobs', cache=True)
-def jobs(store):
-    df = store['jobs']
+def jobs(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('jobs'))
     return df
 
 
 @orca.table('establishments', cache=True)
-def establishments(store):
-    df = store['establishments']
+def establishments(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('establishments'))
     return df
 
 
 @orca.table('households', cache=True)
-def households(store):
-    df = store['households']
+def households(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('households'))
     return df
 
 
 @orca.table('persons', cache=True)
-def persons(store):
-    df = store['persons']
+def persons(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('persons'))
     return df
 
 
 @orca.table('rentals', cache=True)
-def rentals(store):
-    craigslist = store['craigslist']
-    craigslist['rent'] = craigslist['rent'].astype(float)
-    craigslist.rent[craigslist.rent < 100] = 100.0
-    craigslist.rent[craigslist.rent > 10000] = 10000.0
-
-    craigslist.rent_sqft[craigslist.rent_sqft < .2] = .2
-    craigslist.rent_sqft[craigslist.rent_sqft > 50] = 50.0
-    return craigslist
+def rentals(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('rentals'))
+    df['rent'] = df['rent'].astype(float)
+    df.rent[df.rent < 100] = 100.0
+    df.rent[df.rent > 10000] = 10000.0
+    df.rent_sqft[df.rent_sqft < .2] = .2
+    df.rent_sqft[df.rent_sqft > 50] = 50.0
+    return df
 
 
 @orca.table('units', cache=True)
-def units(store):
-    df = store['units']
+def units(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('units'))
     df.index.name = 'unit_id'
     return df
 
 
-# @orca.table('nodessmall', cache=True)
-# def nodessmall(store):
-#     df = store['nodessmall']
-#     return df
+# Tables from Jayne Chang
+
+# Append AM peak UrbanAccess transit accessibility variables to parcels table
+
+parcels = orca.get_table('parcels').to_frame()
+am_acc = pd.read_csv(
+    './data/access_indicators_ampeak.csv', dtype={'block_id': str})
+am_acc.block_id = am_acc.block_id.str.zfill(15)
+parcels_with_acc = parcels.merge(
+    am_acc, how='left', on='block_id').reindex(
+    index = parcels.index) # reorder to align with parcels table
+
+for acc_col in set(parcels_with_acc.columns) - set(parcels):
+    # fill NA with median value
+    orca.add_column('parcels',acc_col,
+        parcels_with_acc[acc_col].fillna(parcels_with_acc[acc_col].median()))
 
 
-# @orca.table('edgessmall', cache=True)
-# def edgessmall(store):
-#     df = store['edgessmall']
-#     return df
-
-
-# @orca.table('nodeswalk', cache=True)
-# def nodeswalk(store):
-#     df = store['nodeswalk']
-#     return df
-
-
-# @orca.table('edgeswalk', cache=True)
-# def edgeswalk(store):
-#     df = store['edgeswalk']
-#     return df
-
-
-# @orca.table('nodesbeam', cache=True)
-# def nodesbeam(store):
-#     df = store['nodesbeam']
-#     return df
-
-
-# @orca.table('edgesbeam', cache=True)
-# def edgesbeam(store):
-#     df = store['edgesbeam']
-#     return df
+# Tables from Emma
+@orca.table('skims', cache=True)
+def skims(s3_input_data_url):
+    df = pd.read_parquet(s3_input_data_url.format('skims'))
+    return df
 
 
 # Broadcasts, a.k.a. merge relationships
