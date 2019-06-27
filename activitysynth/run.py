8912164fea6a63d53a0e2f1d5aa3c7ff4dc11406
Max Gardner <gardner.max@gmail.com>
2019-06-27 12:49:58 -0700

    quick fixes before PR

--- activitysynth/run.py
+++ activitysynth/run.py
@@ -18,11 +18,6 @@ scenario = None
 accessibilities_mode = 'compute'
 data_out = './output/model_data_output.h5'
 output_store = False
-# data_mode = 'local'
-# io_bucket = 'urbansim-outputs'
-# beam_bucket = 'urbansim-beam'
-# write_to_s3 = False
-# output_file_format = 'csv'
 input_file_format = 'csv'
 formattable_fname_dict = {
     'parcels': 'parcels.{0}',
@@ -63,57 +58,6 @@ def format_fname_dict(formattable_fname_dict, format='csv'):
     return formatted_dict
 
 
-# def format_s3_url(
-#         bucket_name, today_str, year, table_name, output_file_format):
-#     s3_url = 's3://{0}/{1}/{2}/{3}.{4}'.format(
-#         bucket_name, today_str, year, table_name, output_file_format)
-#     return s3_url
-
-
-# def send_output_to_s3(
-#         output_tables, io_bucket, beam_bucket, year, scenario,
-#         output_file_format):
-
-#     today_str = datetime.now().strftime("%d%B%Y")
-
-#     # save urbansim tables to s3
-#     s3 = s3fs.S3FileSystem(anon=False)
-
-#     for table_name in output_tables:
-#         print(table_name)
-#         table = orca.get_table(table_name)
-#         df = table.to_frame(table.local_columns)
-
-#         if scenario:
-#             table_name = scenario + '/' + table_name
-
-#         s3_url = format_s3_url(
-#             io_bucket, today_str, year, table_name, output_file_format)
-
-#         with s3.open(s3_url, 'w') as f:
-#             if output_file_format == 'parquet':
-#                 df.to_parquet(f, engine='pyarrow')
-#             elif output_file_format == 'csv':
-#                 df.to_csv(f)
-
-#     # save activity plans to s3
-#     table_name = 'activity_plans'
-#     print(table_name)
-#     plans = orca.get_table('plans').to_frame()
-
-#     if scenario:
-#         table_name = scenario + '/' + table_name
-
-#     s3_url = format_s3_url(
-#         beam_bucket, today_str, year, table_name, output_file_format)
-
-#     with s3.open(s3_url, 'w') as f:
-#         if output_file_format == 'parquet':
-#             plans.to_parquet(f)
-#         elif output_file_format == 'csv':
-#             plans.to_csv(f)
-
-
 if __name__ == "__main__":
 
     # parse runtime arguments
@@ -137,28 +81,11 @@ if __name__ == "__main__":
         '-d', action='store_true', dest='data_out',
         help='full filepath for output data')
 
-    # parser.add_argument(
-    #     "--ouput-file-format", "-o", dest='output_file_format',
-    #     action="store",
-    #     help="options: h5, csv, parquet")
-    # parser.add_argument(
-    #     '--scenario', '-s', action='store', dest='scenario',
-    #     help='specify which scenario to run')
     parser.add_argument(
         "--access-vars-mode", "-a", help="option: compute, stored",
         action="store", dest='accessibilities_mode')
 
     options = parser.parse_args()
-
-    # if options.year:
-    #     year = options.year
-    # print("The year is {0}.".format(year))
-
-    # if options.scenario:
-    #     scenario = options.scenario
-
-    # if options.data_mode:
-    #     data_mode = options.data_mode
 
     if options.input_file_format:
         input_file_format = options.input_file_format
@@ -172,24 +99,8 @@ if __name__ == "__main__":
     else:
         data_out = None
 
-    # if options.output_file_format:
-    #     output_file_format = options.output_file_format
-
-    # if options.write_to_s3:
-    #     write_to_s3 = options.write_to_s3
-
     if options.accessibilities_mode:
         accessibilities_mode = options.accessibilities_mode
-
-    # if data_mode == 'local':
-    #     input_data_dir = local_data_dir
-    # elif data_mode == 's3':
-    #     input_data_dir = 's3://{0}/'.format(io_bucket)
-
-    # input_data_dir = input_data_dir + '{0}/'.format(str(year))
-
-    # if scenario:
-    #     input_data_dir += '{0}/'.format(scenario)
 
     if options.input_data_dir:
         input_data_dir = options.input_data_dir
@@ -275,8 +186,3 @@ if __name__ == "__main__":
         out_base_local=True,
         out_run_tables=output_tables,
         out_run_local=True)
-
-    # if write_to_s3:
-    #     send_output_to_s3(
-    #         output_tables, io_bucket, beam_bucket, year, scenario,
-    #         output_file_format)
